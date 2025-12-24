eab1ca80dbe21bcaa9bc296553a4ad8b825ab25d
Денис <respectabelen@MacBook-Air-Denis.local>
2025-12-24 12:38:46 +0200

    second task

--- main1.c
+++ main1.c
@@ -4,6 +4,7 @@
 #include <locale.h>
 #include <math.h>
 
+/*
 void fill(int *, int, int);
 void print(int *, int, int);
 int min(int *, int, int);
@@ -69,4 +70,77 @@ int max(int *pm, int rows, int cols) {
 
     }
     return max1;
+}
+*/
+void fill(int *, int, int);
+void print(int *, int, int);
+int sum2(int *, int, int);
+int sum3(int *, int, int, int);
+
+int main() {
+int n = 5, m = 5;
+
+int result, result1;
+int A[n][m];
+int c;
+
+printf("\n введите столбец(1-5) = ");
+scanf("%i", &c);
+c--;
+
+srand(time(NULL));
+
+fill(A[0], n, m);
+print(A[0], n, m);
+result = sum2(A[0], n, m);
+result1 = sum3(A[0], n, m, c);
+
+   printf("\n Сумма квадратов элементов второй строки массива = %i\n", result);
+   printf("\n Сумма квадратов элементов %i-го столбца = %i\n", c+1, result1);
+}
+
+void fill (int *pm, int rows, int cols) {
+int i, j;
+for(i = 0; i < rows; i++) {
+   printf("\n");
+   for(j = 0; j < cols; j++) {
+   pm[i * cols + j] = rand() % 10 - 5;
+      }
+   }
+}
+
+void print (int *pm, int rows, int cols) {
+int i, j;
+for(i = 0; i < rows; i++) {
+   printf("\n");
+   for(j = 0; j < cols; j++) {
+   printf("%4i", pm[i * cols + j]);
+      }
+   }
+}
+
+int sum2(int *pm, int rows, int cols) {
+int sum = 0;
+int row = 1;
+
+    for(int j = 0; j < cols; j++) {
+        int x = pm[row * cols + j];
+        sum += x * x;
+      }
+   return sum;
+}
+
+int sum3(int *pm, int rows, int cols, int c) {
+    int sum = 0;
+
+    if (c < 0 || c >= cols) {
+        printf("\n такого столбца не существует \n");
+        return 0;
+    }
+
+    for (int i = 0; i < rows; i++) {
+        int x = pm[i * cols + c];
+        sum += x * x;
+    }
+    return sum;
 }
